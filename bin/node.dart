abecb73c167232f98d4a8262189ba7614e888fce
Tim Whiting <tim@whitings.org>
2020-05-21 12:13:39 -0600

    update

--- bin/node.dart
+++ bin/node.dart
@@ -6,19 +6,22 @@ import 'package:dartros/dartros.dart';
 import 'package:std_msgs/msgs.dart';
 import 'package:dartros/src/utils/tcpros_utils.dart';
 
-void main(List<String> args) {
-  final node = Node('test_node');
+void main(List<String> args) async {
+  final node = await initNode('test_node', args);
   final str_msg = StringMessage(data: 'hello');
 
   final writer = ByteDataWriter(endian: Endian.little);
   createPubHeader(writer, '/rostopic_4767_1316912741557', str_msg.md5sum,
       str_msg.fullType, true, str_msg.messageDefinition);
-  serializeMessage(writer, str_msg);
-  print(HEX.encode(writer.toBytes().toList()));
-  final reader = ByteDataReader(endian: Endian.little);
-  reader.add(writer.toBytes());
-  final header = parseTcpRosHeader(reader);
+  final header = parseTcpRosHeader(TCPRosChunk(writer.toBytes()));
   print(header);
-  final message = deserializeMessage<StringMessage>(reader);
+
+  final messageWriter = ByteDataWriter(endian: Endian.little);
+  serializeMessage(messageWriter, str_msg);
+  print(HEX.encode(messageWriter.toBytes().toList()));
+  final reader = ByteDataReader(endian: Endian.little);
+  reader.add(messageWriter.toBytes());
+  final message =
+      deserializeMessage<StringMessage>(reader, std_msgs.StringMessage);
   print(message.data);
 }
--- bin/pub.dart
+++ bin/pub.dart
@@ -1,10 +1,10 @@
 import 'package:dartros/dartros.dart';
 import 'package:std_msgs/msgs.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   //TODO: Change to node handle
-  final node = Node('test_node');
+  final node = await initNode('test_node', args);
   final str_msg = StringMessage(data: 'hello');
   final pub = node.advertise<StringMessage>('/chatter', std_msgs.StringMessage);
-  pub.publish(str_msg);
+  pub.publish(str_msg, 1);
 }
